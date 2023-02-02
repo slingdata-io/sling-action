c527b6d7454a4bbdef700bb32404beca27c3654e
Fritz Larco <flarco@live.com>
2023-02-02 08:53:57 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,4 @@ ls -l /tmp/work
 
 echo ' >>> step >>>'
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v /tmp/work:/work -w /work --env-file /tmp/work/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "ls -l /work"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" --network $INPUT_DOCKER_NETWORK -v /tmp/work:/work -w /work --env-file /tmp/work/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "ls -l /work"
