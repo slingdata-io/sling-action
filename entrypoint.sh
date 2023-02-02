a06bf5df37622f54572b187ec9a42c206dc28d68
Fritz Larco <flarco@live.com>
2023-02-02 08:37:33 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
 
 ls -l $GITHUB_WORKSPACE
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE:/work -w /work --env-file /tmp/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "cd /work && sling $INPUT_COMMAND"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE:/work -w /work --env-file /tmp/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "cd /work && ls -l && sling $INPUT_COMMAND"
