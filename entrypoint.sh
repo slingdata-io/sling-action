4b8c8ebbe6573cdf560029b09933c2de43f12a85
Fritz Larco <flarco@live.com>
2023-02-02 08:02:22 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-sh -c "sling $INPUT_COMMAND"
+export SLING_PROJECT=$INPUT_PROJECT_ID
+export SLING_API_KEY=$INPUT_API_KEY
+export SLING_POOL=true
+export SLING_LOADED_AT_COLUMN=true
+
+env > /tmp/.env
+
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" --env-file /tmp/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "sling $INPUT_COMMAND"
