690a8cb71cc6df211a86dff63ccc39757ac49207
Fritz Larco <flarco@live.com>
2023-02-02 11:10:01 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,10 @@ export SLING_LOADED_AT_COLUMN=true
 
 cp -r $GITHUB_WORKSPACE /tmp/work
 
-echo "INPUT_PROJECT_ID=$INPUT_PROJECT_ID"
-
 env > /tmp/work/.env
 
 ls -l /tmp/work
 
 echo ' >>> step >>>'
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v /tmp/work:/sling-work -w /sling-work --env-file /tmp/work/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "ls -l /sling-work"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v /tmp/work/:/work -w /work --env-file /tmp/work/.env --entrypoint=sh slingdata/sling:$INPUT_VERSION -c "ls -l /work"
