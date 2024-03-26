055e024aeee1809d231f4da000c986ee68066ae1
Fritz Larco <flarco@live.com>
2024-03-26 07:04:33 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,10 @@ export SLING_LOADED_AT_COLUMN=true
 if [ "$INPUT_VERSION" != "latest" ]; then
   # download sling version
   echo "Downloading version $INPUT_VERSION"
-  wget -q https://ocral.nyc3.cdn.digitaloceanspaces.com/slingdata.io/dist/$INPUT_VERSION/sling-linux
-  mv sling-linux /usr/local/bin/sling
+  wget -q https://github.com/slingdata-io/sling-cli/releases/download/v$INPUT_VERSION/sling_linux_amd64.tar.gz
+  tar -xf sling_linux_amd64.tar.gz
+  rm -f /usr/local/bin/sling
+  mv sling /usr/local/bin/sling
   chmod 755 /usr/local/bin/sling
 fi
 
